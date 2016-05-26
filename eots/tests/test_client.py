3f9414bf9b8d11ede8bf20b9447fde12280dc4da
Sinan Midillili <sinanm89@gmail.com>
2016-05-26 00:58:07 -0400

    Update test_client.py
    added tests for utf8 support

--- eots/tests/test_client.py
+++ eots/tests/test_client.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Test the eots client module."""
 from mock import patch, Mock
 
 from twisted.trial import unittest
@@ -5,17 +9,38 @@ from eots.client import RESTClient, EOTSResponse
 
 
 class RESTClientTest(unittest.TestCase):
+    """Test basic authentication in eots."""
+
     @patch('eots.client.treq')
     def test_auth_is_used_when_passed(self, treq_mock):
+        """Test succesful user authentication."""
         username, password = 'user', 'hunter2'
         client = RESTClient('http://base/', auth=(username, password))
         client.retrieve('resource', 1)
         args, kwargs = treq_mock.request.call_args
         self.assertTrue(kwargs['auth'] == (username, password))
 
+    @patch('eots.client.treq')
+    def test_utf8(self, treq_mock):
+        """Test succesful user authentication."""
+        username, password = 'user', 'hunter2'
+        client = RESTClient(
+            'http://base/',
+            auth=(username, password),
+        )
+        client.list(
+            'resource',
+            params={'search': u"pijamalı hasta yağız şoföre çabucak güvendi"}
+        )
+        args, kwargs = treq_mock.request.call_args
+        self.assertTrue(kwargs['auth'] == (username, password))
+
 
 class EOTSResponseTest(unittest.TestCase):
+    """Test the eots reponse for a request."""
+
     def test_get_complete_content(self):
+        """Test succesful content acquisition."""
         response = Mock()
         response.content = "none"
         er = EOTSResponse(response)
